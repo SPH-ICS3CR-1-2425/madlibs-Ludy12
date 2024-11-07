d736a07bc0fb3d9973772ce5feebe3b05ce34ab5
Ludy12 <domingos.joao@stu.ocsb.ca>
2024-11-07 10:23:42 -0500

    done

--- Main.py
+++ Main.py
@@ -1,10 +1,20 @@
 #Ask the user for at least ten questions
 #Read in each answer and store it in a variable
 #Print out the result
-print("Give me a verb")
-verb1 = input()
-verb2 = input(" Give me another verb past sentence: ")
-print("On december 10th while i was ",verb1,"  and i ",verb2," in the sky and i was wondering ")
+print("Give me a name")
+name = input()  # Fixed variable name here
+
+verb1 = input("Give me a drink: ")
+name2 = input("Give me another name: ")  # Fixed variable name here
+verb2 = input("Give me playground/sport: ")
+verb3 = input("You prefer good or bad:  ")
+print("One day", name, "was walking and drinking",verb1,"and he saw", name2, "playing", verb2," and",name2,"  was so",verb3," that everyone was recording him so",name,"decided to also play", verb2,  )
+
+
+
+
+
+
 
 
 
